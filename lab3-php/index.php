d44068449583969ced677742be2318bd7c314f92
archilkarchava <archil1997@gmail.com>
2017-09-22 22:57:28 +0500

    php версия

---
+++ lab3-php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <title>Lab3</title>
+</head>
+<body class="container vertical-center">
+    <div class="login-page">
+        <div class="form">
+            <form class="register-form">
+                <input type="text" placeholder="Имя пользователя"/> <br>
+                <input type="password" placeholder="Пароль"/> <br>
+                <input type="tel" placeholder="Телефон"> <br>
+                <input type="email" placeholder="Email"/> <br>
+                <input type="submit" value="Отправить">
+                <p class="message">Уже зарегестрирован? <a href="#">Залогиньтесь</a></p>
+            </form>
+<!--            <form class="login-form">
+                <input type="text" placeholder="Имя пользователя"/> <br>
+                <input type="password" placeholder="Пароль"/> <br>
+                <input type="submit" value="Отправить">
+                <p class="message">Еще не зарегестрирован? <a href="#">Зарегестрироваться</a></p>
+            </form>-->
+        </div>
+    </div>
+</body>
+</html>
