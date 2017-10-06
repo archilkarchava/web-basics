2532e1ccce11d4e78e03fff165d1c87bc0abff9a
archilkarchava <archil1997@gmail.com>
2017-10-06 23:08:14 +0500

    Страница регистрации готова.

--- lab3-php/index.php
+++ lab3-php/index.php
@@ -1,32 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>lab3</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="main.js"></script>
-    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <title>Lab3</title>
+    <link rel="stylesheet" href=main.css
 </head>
-<body class="container vertical-center">
-    <div class="login-page">
-        <div class="form">
-            <form class="register-form">
-                <input type="text" placeholder="Имя пользователя"/> <br>
-                <input type="password" placeholder="Пароль"/> <br>
-                <input type="tel" placeholder="Телефон"> <br>
-                <input type="email" placeholder="Email"/> <br>
-                <input type="submit" value="Отправить">
-                <p class="message">Уже зарегестрирован? <a href="#">Залогиньтесь</a></p>
-            </form>
-<!--            <form class="login-form">
-                <input type="text" placeholder="Имя пользователя"/> <br>
-                <input type="password" placeholder="Пароль"/> <br>
-                <input type="submit" value="Отправить">
-                <p class="message">Еще не зарегестрирован? <a href="#">Зарегестрироваться</a></p>
-            </form>-->
-        </div>
+<body class="container">
+    <div>
+        <form class="centered" action="" method="post">
+            <input type="text" placeholder="Имя пользователя" id="" autofocus>
+            <br>
+            <input type="password" placeholder="Пароль" id="">
+            <br>
+            <input type="tel" pattern="^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$" placeholder="Телефон" id=""> <br>
+            <input type="email" placeholder="Email" id=""> <br> <br>
+            <button type="submit">Отправить</button>
+        </form>
     </div>
 </body>
 </html>
